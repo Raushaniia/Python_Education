e015cfc5724ede6935940142fdd254db370f1f51
rushaniia.badrutdinova <r.badrutdinova@akvelon.com>
2019-04-26 15:25:10 +0300

    Add files via upload

---
+++ Build-in_constants.py
@@ -0,0 +1,13 @@
+#The false value of the bool type. Assignments to False are illegal and raise a SyntaxError
+False
+#The true value of the bool type. Assignments to True are illegal and raise a SyntaxError.
+True
+#The sole value of the type NoneType. None is frequently used to represent the absence of a value, as when default arguments are not passed to a function. Assignments to None are illegal and raise a SyntaxError.
+None
+#Special value which should be returned by the binary special methods (e.g. __eq__(), __lt__(), __add__(), __rsub__(), etc.) to indicate that the operation is not implemented with respect to the other type; may be returned by the in-place binary special methods (e.g. __imul__(), __iand__(), etc.) for the same purpose. Its truth value is true.
+NotImplemented
+#The same as the ellipsis literal “...”. Special value used mostly in conjunction with extended slicing syntax for user-defined container data types.
+Ellipsis
+#This constant is true if Python was not started with an -O option. See also the assert statement.
+__debug__
+
